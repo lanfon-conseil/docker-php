2b7dc3e5e099b274aed051cb0a70f0de54ab52bc
Gregory <gregory.sicardi@lanfon-conseil.com>
2023-04-12 14:25:08 +0200

    add php 8.1.13

--- build_all.sh
+++ build_all.sh
@@ -42,3 +42,7 @@ docker build -t lanfonconseil/php:7.4-cli 7.4/cli
 docker build -t lanfonconseil/php:8.1-apache 8.1/apache
 docker build -t lanfonconseil/php:8.1-apache-xdebug 8.1/apache-xdebug
 docker build -t lanfonconseil/php:8.1-cli 8.1/cli
+
+docker build -t lanfonconseil/php:8.1.13-apache 8.1.13/apache
+docker build -t lanfonconseil/php:8.1.13-apache-xdebug 8.1.13/apache-xdebug
+docker build -t lanfonconseil/php:8.1.13-cli 8.1.13/cli
